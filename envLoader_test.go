64c7bc3201afa840830f4e6771d74894981c80da
nikos <nikos.efthias@gmail.com>
2017-07-13 02:38:28 +0400

    test env variables with spaces in the file

--- envLoader_test.go
+++ envLoader_test.go
@@ -57,3 +57,27 @@ func TestLoadCustomFilename(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestLoadWithSpaces(t *testing.T) {
+	f, err := os.OpenFile(".env.file", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0775)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, v := range cases {
+		fmt.Fprintf(f, "%s   ,   %s    \n", v[0], v[1])
+		//put spaces around
+	}
+	f.Close()
+
+	if err:=Load(".env.file");nil!=err{
+		t.Fatal(err)
+	}
+	for _,kv:=range cases {
+		if os.Getenv(kv[0]) != kv[1] {
+			t.Fail()
+		}
+	}
+	if err:=os.Remove(".env.file");nil!=err{
+		t.Fail()
+	}
+}
